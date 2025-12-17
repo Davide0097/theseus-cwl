0d00d9846d9fc9fcbf6ac9c879807dbaf5b8f478
Davide0097 <davidefxfx@gmail.com>
2025-12-17 12:31:58 +0100

    chore: fix ui react editor build

--- packages/ui-react-editor/src/ui/cwl-code-editor.tsx
+++ packages/ui-react-editor/src/ui/cwl-code-editor.tsx
@@ -1,1 +1,30 @@
- 
+import { yaml } from "@codemirror/lang-yaml";
+import { EditorView, lineNumbers } from "@uiw/react-codemirror";
+
+import CodeMirror from "@uiw/react-codemirror";
+
+const CodeMirror_ = CodeMirror as React.MemoExoticComponent<
+  React.ForwardRefExoticComponent<any & React.RefAttributes<Element>>
+>;
+
+export type CwlCodeEditorProps = {
+  value: string;
+  onChange: () => void;
+};
+
+export const CwlCodeEditor = (props: CwlCodeEditorProps) => {
+  const { value } = props;
+  return (
+    <CodeMirror_
+      value={value}
+      height="100%"
+      theme="dark"
+      extensions={[
+        yaml(),
+        lineNumbers(),
+        // ide?.cwlCodeEditor.options?.wrap ? EditorView.lineWrapping : [],
+      ]}
+      //   onChange={(c) => ide?.cwlIdeStore.setAST(c)}
+    />
+  );
+};
